1a2e0b3705490d596569d3d504027f806c69c8f2
hjiunliau <hjiunliau@liontravel.com>
2018-08-25 22:22:25 +0800

    POST呼叫API

--- Program.cs
+++ Program.cs
@@ -73,29 +73,55 @@ namespace TEST
 
         private static void Main(string[] args)
         {
-            var jjj = "資訊陳姿穎".Replace("資訊", string.Empty);
-            var hh = testStr;
-            testStr = "方道筌";
-            var hh2 = testStr;
-            var jij455i = CountryList ?? new List<string>();
-            var ffr = Convert.ToString((CountryList ?? new List<string> { "0" }).Select(n => int.Parse(n)).Aggregate((start, next) => start + next), 2).PadLeft(5, '0');
+            #region - 呼叫API -
+            //POST 
+            #region - 使用WebClient -
+            var jsonText = new JavaScriptSerializer().Serialize(
+                new Dictionary<string, object>()
+                {
+                    { "ClientSysId", "123456" }
+                });
 
-            var uhuhfiroejiofre = Convert.ToInt32(true);
-            var uhuhfiroerw4r3wejiofre = Convert.ToInt32(false);
-            var ttt = BitConverter.GetBytes(true);
-            var fff = BitConverter.GetBytes(false);
-            string aaStr = "aa";
-            string eee = (-(11 - aaStr.Length)).ToString();
-            string endResult = string.Format("{0}{1," + (-(11 - aaStr.Length)) + "}{2}", aaStr, string.Empty, "bbb");
-            string emptyStr = "{0}{1," + eee + "}{2}";
-            string str = string.Format(emptyStr, aaStr, string.Empty, "bbb");
+            WebClient client = new WebClient { Encoding = Encoding.UTF8 };
+            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
 
-            var hu = HttpUtility.UrlEncode("http://upub.liontravel.com.tw/Pub/SignForm");
-            string jj = "123456";
-            var huhuhgut = jj.Where(c => c == '6').ToList();
+            //呼叫API,回傳結果為byte[]
+            var responseWebClient = client.UploadData("http://127.0.0.1:6697/CSV/InsertCustApiLog", "POST", Encoding.UTF8.GetBytes(jsonText));
+            var apiResult = Encoding.UTF8.GetString(responseWebClient);//API回傳資料轉成字串
+            #endregion
 
-            bool a = false;
-            var aaa = a.ToString();
+            #region - 使用HttpWebRequest -
+            HttpWebRequest requestt = WebRequest.Create("http://127.0.0.1:6697/CSV/InsertCustApiLog") as HttpWebRequest;
+            if (requestt != null)
+            {
+                //傳入參數
+                var jsonParaa = new JavaScriptSerializer().Serialize(
+                    new Dictionary<string, object>()
+                    {
+                        { "ClientSysId", "123456" }
+                    });
+                byte[] bs = Encoding.UTF8.GetBytes(jsonParaa);
+
+                requestt.Method = "POST";
+                requestt.KeepAlive = true;
+                requestt.ContentType = "application/json";
+                requestt.ContentLength = bs.Length;
+
+                //寫入POST Body 資料
+                using (Stream reqStream = requestt.GetRequestStream())
+                {
+                    reqStream.Write(bs, 0, bs.Length); //寫入資料的時候就是呼叫API
+                    //reqStream.Flush();
+                }
+                using (WebResponse response = requestt.GetResponse())
+                {
+                    StreamReader sr = new StreamReader(response.GetResponseStream());
+                    string resultt = sr.ReadToEnd(); //API回傳資料
+                    sr.Close();
+                }
+            }
+            #endregion
+            #endregion
 
             #region - LDAP -
             //string domainNM = string.Empty;
